ea96d6403d07176777e6d63ac6dbcec6c4823442
foolishneo <foolishneo@gmail.com>
2015-11-13 02:46:15 +1100

    add db string

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://<dbuser>:<dbpassword>@ds053954.mongolab.com:53954/heroku_ljk89xvz',
+  db: 'mongodb://<foolishneo-heroku-app-0310>:<Welcome141>@ds053954.mongolab.com:53954/heroku_ljk89xvz',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
